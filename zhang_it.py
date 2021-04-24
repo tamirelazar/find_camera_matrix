c1808056354d63fe5b68bae2de307f6bfab77f6f
fastamir <tamir.elazar@mail.huji.ac.il>
2021-04-24 15:59:09 +0300

    balagan

--- zhang_it.py
+++ zhang_it.py
@@ -7,6 +7,7 @@ import numpy as np
 IMAGE_POINTS = np.int0([(0, 0), (0, 1), (1, 0), (1, 1)])
 CIRCULAR_POINTS = np.array([[1, 1j, 0], [1, -1j, 0]])
 
+
 def sort_sad(corners):
     corner_sums = []
     for corner in corners:
@@ -15,18 +16,24 @@ def sort_sad(corners):
 
     max_idx = np.argmax(corner_sums)
     min_idx = np.argmin(corner_sums)
-    sorted = [[], [], [], []]
+    mx = corners[max_idx]
+    mn = corners[min_idx]
+    sort = [mn]
+    middle = []
     for idx, corner in enumerate(corners):
-        if idx == max_idx:
-            sorted[3] = corner[0]
-        elif idx == min_idx:
-            sorted[0] = corner[0]
-        elif corner[0, 0] > corner[0, 1]:
-            sorted[2] = corner[0]
+        if idx == max_idx or idx == min_idx:
+            pass
         else:
-            sorted[1] = corner[0]
+            middle.append(corner)
+    if middle[0][0, 1] > middle[1][0, 1]:
+        sort.append(middle[1])
+        sort.append(middle[0])
+    else:
+        sort.append(middle[0])
+        sort.append(middle[1])
+    sort.append(mx)
 
-    return np.int0(sorted)
+    return sort
 
 
 def get_warped_circular(img):
@@ -38,7 +45,18 @@ def get_warped_circular(img):
     corners1 = cv2.goodFeaturesToTrack(gray, 4, 0.01, 10)
     corners1 = np.int0(corners1)
 
-    homo, _ = cv2.findHomography(corners1, corners2)
+    # for corner in corners1:
+    #     print(corner)
+    #     x, y = corner.ravel()
+    #     cv2.circle(img, (x, y), 3, 255, -1)
+    #
+    # cv2.imshow('Corner', img)
+    #
+    # cv2.waitKey(0)
+    corners1 = np.float32(sort_sad(corners1))
+    homo, _ = cv2.findHomography(corners1, corners2, cv2.RANSAC, 5.0)
+
+    return homo
 
     circular = CIRCULAR_POINTS.copy().transpose()
     circular_warped_a = np.matmul(homo, circular[:, 0])
@@ -54,10 +72,63 @@ def calculate_conic(points):
     Mat = np.vstack([x ** 2, x * y, y ** 2, x, y]).T
     fullSolution = np.linalg.lstsq(Mat, np.ones(x.size), rcond=None)
     (a, b, c, d, e) = fullSolution[0]
+
     ret = np.array([[a, b / 2, d / 2],
            [b / 2, c, e / 2],
            [d / 2, e / 2, 1]])
     return ret
+
+
+def v(p, q, H):
+
+    return np.array([
+        H[0, p] * H[0, q],
+        H[0, p] * H[1, q] + H[1, p] * H[0, q],
+        H[1, p] * H[1, q],
+        H[2, p] * H[0, q] + H[0, p] * H[2, q],
+        H[2, p] * H[1, q] + H[1, p] * H[2, q],
+        H[2, p] * H[2, q]
+    ])
+
+
+def get_camera_intrinsics(homographies):
+
+    h_count = len(homographies)
+
+    vec = []
+
+    for i in range(0, h_count):
+        curr = np.reshape(homographies[i], (3, 3))
+
+        vec.append(v(0, 1, curr))
+        vec.append(v(0, 0, curr) - v(1, 1, curr))
+
+    vec = np.array(vec)
+
+    b = np.linalg.lstsq(
+        vec,
+        np.zeros(h_count * 2),
+    )[-1]
+
+    w = b[0] * b[2] * b[5] - b[1]**2 * b[5] - b[0] * b[4]**2 + 2 * b[1] * b[3] * b[4] - b[2] * b[3]**2
+    d = b[0] * b[2] - b[1]**2
+
+    # if (d < 0):
+    #     d = 0.01
+    d = -d
+
+    #
+    alpha = np.sqrt(w / (d * b[0]))
+    beta = np.sqrt(w / d**2 * b[0])
+    gamma = np.sqrt(w / (d**2 * b[0])) * b[1]
+    uc = (b[1] * b[4] - b[2] * b[3]) / d
+    vc = (b[1] * b[3] - b[0] * b[4]) / d
+
+    return np.array([
+        [alpha, gamma, uc],
+        [0,     beta,  vc],
+        [0,     0,      1]
+        ])
 
 
 if __name__ == "__main__":
@@ -75,9 +146,15 @@ if __name__ == "__main__":
         raise Exception("Didn't get 3 pictures from " + images_path)
 
     warped_circular = []
+    homo_set = []
     for image in images:
         im = cv2.imread(images_path + "/" + image)
-        warped_circular += (get_warped_circular(im))
+        homo_set.append(get_warped_circular(im))
+        # warped_circular += (get_warped_circular(im))
+
+    intrinsic = get_camera_intrinsics(homo_set)
+    print(intrinsic)
+    exit()
 
     warped_circular = np.array(warped_circular)
 
@@ -86,7 +163,6 @@ if __name__ == "__main__":
     a, s_mat, b = np.linalg.svd(conic)
     new_conic = np.matmul(a, s_mat, b)
     # print(new_conic)
-
 
     camera_mat = np.linalg.cholesky(conic)
     # warped = cv2.warpPerspective(image, homo, (1, 1))
