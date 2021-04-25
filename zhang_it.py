5f456a96af313c54fd448b081326acca2830f768
fastamir <tamir.elazar@mail.huji.ac.il>
2021-04-25 12:59:20 +0300

    cleaned code a little

--- zhang_it.py
+++ zhang_it.py
@@ -45,90 +45,36 @@ def get_warped_circular(img):
     corners1 = cv2.goodFeaturesToTrack(gray, 4, 0.01, 10)
     corners1 = np.int0(corners1)
 
-    # for corner in corners1:
-    #     print(corner)
-    #     x, y = corner.ravel()
-    #     cv2.circle(img, (x, y), 3, 255, -1)
-    #
-    # cv2.imshow('Corner', img)
-    #
-    # cv2.waitKey(0)
     corners1 = np.float32(sort_sad(corners1))
-    homo, _ = cv2.findHomography(corners1, corners2, cv2.RANSAC, 5.0)
-
-    return homo
+    homography, _ = cv2.findHomography(corners2, corners1, cv2.RANSAC, 5.0)
 
     circular = CIRCULAR_POINTS.copy().transpose()
-    circular_warped_a = np.matmul(homo, circular[:, 0])
-    circular_warped_b = np.matmul(homo, circular[:, 1])
+    circular_warped_a = np.matmul(homography, circular[:, 0])
+    circular_warped_b = np.matmul(homography, circular[:, 1])
 
     return [circular_warped_a, circular_warped_b]
 
 
 def calculate_conic(points):
-    x = points[:, 0]
-    y = points[:, 1]
+    """
+    Given six points, calculates the conic that fits all of them.
+    :param points: a points array of size (6,2)
+    :return: matrix representation of the fitted conic
+    """
+    calc_conic = np.zeros(shape=(6, 6), dtype=complex)
+    for i in range(6):
+        point = points[i]
+        calc_conic[i] = (point[0]**2, point[0]*point[1],
+                         point[1]**2, point[0]*point[2],
+                         point[1]*point[2], point[2]**2)
 
-    Mat = np.vstack([x ** 2, x * y, y ** 2, x, y]).T
-    fullSolution = np.linalg.lstsq(Mat, np.ones(x.size), rcond=None)
-    (a, b, c, d, e) = fullSolution[0]
+    u, s, vh = np.linalg.svd(calc_conic)
+    (a, b, c, d, e, f) = vh[-1]
 
     ret = np.array([[a, b / 2, d / 2],
            [b / 2, c, e / 2],
-           [d / 2, e / 2, 1]])
+           [d / 2, e / 2, f]])
     return ret
-
-
-def v(p, q, H):
-
-    return np.array([
-        H[0, p] * H[0, q],
-        H[0, p] * H[1, q] + H[1, p] * H[0, q],
-        H[1, p] * H[1, q],
-        H[2, p] * H[0, q] + H[0, p] * H[2, q],
-        H[2, p] * H[1, q] + H[1, p] * H[2, q],
-        H[2, p] * H[2, q]
-    ])
-
-
-def get_camera_intrinsics(homographies):
-
-    h_count = len(homographies)
-
-    vec = []
-
-    for i in range(0, h_count):
-        curr = np.reshape(homographies[i], (3, 3))
-
-        vec.append(v(0, 1, curr))
-        vec.append(v(0, 0, curr) - v(1, 1, curr))
-
-    vec = np.array(vec)
-
-    b = np.linalg.lstsq(
-        vec,
-        np.zeros(h_count * 2),
-    )[-1]
-
-    w = b[0] * b[2] * b[5] - b[1]**2 * b[5] - b[0] * b[4]**2 + 2 * b[1] * b[3] * b[4] - b[2] * b[3]**2
-    d = b[0] * b[2] - b[1]**2
-
-    # if (d < 0):
-    #     d = 0.01
-    d = -d
-
-    #
-    alpha = np.sqrt(w / (d * b[0]))
-    beta = np.sqrt(w / d**2 * b[0])
-    gamma = np.sqrt(w / (d**2 * b[0])) * b[1]
-    uc = (b[1] * b[4] - b[2] * b[3]) / d
-    vc = (b[1] * b[3] - b[0] * b[4]) / d
-
-    return np.array([
-        [alpha, gamma, uc],
-        [0,     beta,  vc],
-        [0,     0,      1]
-        ])
 
 
 if __name__ == "__main__":
@@ -146,36 +92,15 @@ if __name__ == "__main__":
         raise Exception("Didn't get 3 pictures from " + images_path)
 
     warped_circular = []
-    homo_set = []
     for image in images:
         im = cv2.imread(images_path + "/" + image)
-        homo_set.append(get_warped_circular(im))
-        # warped_circular += (get_warped_circular(im))
-
-    intrinsic = get_camera_intrinsics(homo_set)
-    print(intrinsic)
-    exit()
+        warped_circular += (get_warped_circular(im))
 
     warped_circular = np.array(warped_circular)
-
     conic = calculate_conic(warped_circular)
-    # print(conic)
-    a, s_mat, b = np.linalg.svd(conic)
-    new_conic = np.matmul(a, s_mat, b)
-    # print(new_conic)
 
     camera_mat = np.linalg.cholesky(conic)
-    # warped = cv2.warpPerspective(image, homo, (1, 1))
-    #
-    # cv2.imshow('warped', warped)
-
-    # for corner in corners1:
-    #     print(corner)
-    #     x, y = corner.ravel()
-    #     cv2.circle(image, (x, y), 3, 255, -1)
-    #
-    # cv2.imshow('Corner', image)
-
-    # cv2.waitKey(0)
-    #
-    cv2.destroyAllWindows()
+    camera_mat = np.real(camera_mat.T / camera_mat[2, 2])
+    print("\nThe camera calibration matrix calculated from the pictures in " + images_path + "/ is:")
+    print(camera_mat)
+    print("\nCalibrate responsibly!")
